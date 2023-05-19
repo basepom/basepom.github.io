e1aac910c9f28b99465e72182285147eea995152
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2023-05-19 11:47:08 -0700

    Site checkin for project inline-maven-plugin

--- inline-maven-plugin/development/apidocs/package-search-index.js
+++ inline-maven-plugin/development/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.basepom.inline.mojo"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.basepom.inline.mojo"},{"l":"org.basepom.maven.inline_maven_plugin"}];updateSearchResults();
