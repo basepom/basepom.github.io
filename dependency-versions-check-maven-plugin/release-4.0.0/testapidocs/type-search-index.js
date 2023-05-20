6532e4d88a2860b6c146764159257ec509da81d5
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2023-05-19 19:12:20 -0700

    Site checkin for project dependency-versions-check-maven-plugin

---
+++ dependency-versions-check-maven-plugin/release-4.0.0/testapidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.basepom.mojo.dvc","l":"QualifiedNameMatcherTest"},{"p":"org.basepom.mojo.dvc","l":"QualifiedNameTest"}];updateSearchResults();
