9ecf8d6e328f550a2ce0c9f335fc0304fb138542
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2023-05-20 14:32:45 -0700

    Site checkin for project duplicate-finder-maven-plugin Maven Mojo

---
+++ duplicate-finder-maven-plugin/development/testapidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.basepom.mojo.duplicatefinder.artifact"},{"l":"org.basepom.mojo.duplicatefinder.classpath"}];updateSearchResults();
